d70dd0ad6af73d1b42dbc7bf860fdfa3cf3cd42b
javeirsalinas <109610060+javeirsalinas@users.noreply.github.com>
2026-03-28 17:52:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ with st.container():
                 
                 if api_key:
                     genai.configure(api_key=api_key)
-                    model = genai.GenerativeModel('gemini-pro')
+                    model = genai.GenerativeModel('models/gemini-1.5-flash')
                     
                     prompt = f"""
                     Actúa como un Mentor Senior y Emprendedor Serial. 
